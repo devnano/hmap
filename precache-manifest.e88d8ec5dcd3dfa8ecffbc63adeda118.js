c67a2a12cf268a2e405fc70b31b47b7ea7841a7e
Mariano Heredia <albolocura@gmail.com>
2021-12-20 13:18:51 -0300

    Updates

--- precache-manifest.e88d8ec5dcd3dfa8ecffbc63adeda118.js
+++ precache-manifest.e88d8ec5dcd3dfa8ecffbc63adeda118.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c1ebf3268694a687831e644675eb5cf4",
+    "revision": "64de0f8ee9ef337517668af0d8e53f9a",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d9a8a5a7.chunk.css"
   },
   {
-    "revision": "511218f4218502343ecd",
+    "revision": "954c9b8bbb2e13048238",
     "url": "/static/css/main.932cefe1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.587a2e2c.chunk.js"
   },
   {
-    "revision": "511218f4218502343ecd",
-    "url": "/static/js/main.105fbd19.chunk.js"
+    "revision": "954c9b8bbb2e13048238",
+    "url": "/static/js/main.23511e5d.chunk.js"
   },
   {
     "revision": "cd0f15dcdf27c15e9cc0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.976100bf9b7437fb4e295d2468a9a87a.js"
+  "/precache-manifest.e88d8ec5dcd3dfa8ecffbc63adeda118.js"
 );
 
 self.addEventListener('message', (event) => {
